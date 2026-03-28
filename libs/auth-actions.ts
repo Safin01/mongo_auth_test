e253b9ed0208ca529f8304f947efc949663ee807
Safin Sarker <safin@Safins-MacBook-Air.local>
2026-03-29 01:42:18 +0600

    fix sign in issue

--- libs/auth-actions.ts
+++ libs/auth-actions.ts
@@ -3,7 +3,7 @@ import { authClient } from "./auth-client";
 export const signInWithGoogle = async () => {
     await authClient.signIn.social({
         provider: "google",
-        callbackURL : "http://localhost:3000",
+        callbackURL : `${process.env.NEXT_PUBLIC_BASE_URL}`,
         
     })
 }
