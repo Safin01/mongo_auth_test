4866e2f8666c5544a221db792fdbe7395baf70ea
Safin Sarker <safin@Safins-MacBook-Air.local>
2026-03-28 22:54:50 +0600

    Fix argument issue

--- app/api/topics/[id]/route.tsx
+++ app/api/topics/[id]/route.tsx
@@ -1,18 +1,18 @@
-import ConnectMongoDB from "@/libs/mongodb";
-import Topic from "@/models/topic";
-import { NextResponse } from "next/server";
+// import ConnectMongoDB from "@/libs/mongodb";
+// import Topic from "@/models/topic";
+// import { NextResponse } from "next/server";
 
-export async function PUT(request, {params}){
-    const {id} = await params;
-    const {newTitle:title ,newDescription:description} =await request.json();
-    await ConnectMongoDB();
-    await Topic.findByIdAndUpdate(id, {title,description});
-    return NextResponse.json({message:"Topic Updated"}, {status: 200});
-}
+// export async function PUT(request, {params}){
+//     const {id} = await params;
+//     const {newTitle:title ,newDescription:description} =await request.json();
+//     await ConnectMongoDB();
+//     await Topic.findByIdAndUpdate(id, {title,description});
+//     return NextResponse.json({message:"Topic Updated"}, {status: 200});
+// }
 
-export async function GET(request,{params}) {
-    const {id} = await params;
-    await ConnectMongoDB();
-    const topic = await Topic.findOne({_id : id});
-    return NextResponse.json({topic}, {status:200});
-}
+// export async function GET(request,{params}) {
+//     const {id} = await params;
+//     await ConnectMongoDB();
+//     const topic = await Topic.findOne({_id : id});
+//     return NextResponse.json({topic}, {status:200});
+// }
